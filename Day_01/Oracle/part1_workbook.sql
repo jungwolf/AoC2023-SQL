d42bde0f328a2e55cb096d76da00b8945e0f089a
Steven Patenaude <spatenaude@netspend.com>
2023-12-01 13:01:37 -0600

    basically copied example solution

--- Day_01/Oracle/part1_workbook.sql
+++ Day_01/Oracle/part1_workbook.sql
@@ -1,3 +1,12 @@
 -- Sample
-create or replace synonym input_data for day??_part1;
+create or replace synonym input_data for day01_part1;
 
+--let's see if the example solution works
+select
+  sum(to_number(regexp_substr(linevalue,'\d')||regexp_substr(reverse(linevalue),'\d'))) calibration_sum
+from input_data;
+/*
+CALIBRATION_SUM
+55477
+*/
+-- yep
