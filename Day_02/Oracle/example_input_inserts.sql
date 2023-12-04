091bea2eebd106c70e51b95d7f57b35db1fc07af
Steven Patenaude <spatenaude@netspend.com>
2023-12-04 16:01:54 -0600

    example inserts

--- Day_02/Oracle/example_input_inserts.sql
+++ Day_02/Oracle/example_input_inserts.sql
@@ -6,5 +6,10 @@ create table day??_example (lineno number, linevalue varchar2(4000));
 
 --insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'
 --');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green');
 
 commit;
