a01f8af2e38569400f03e6dc06587c09fba38fe5
Steven Patenaude <spatenaude@netspend.com>
2023-12-05 12:31:37 -0600

    make a schematic view, first try at splitting out parts and symbols, needs work

--- Day_03/Oracle/example_input_inserts.sql
+++ Day_03/Oracle/example_input_inserts.sql
@@ -1,12 +1,24 @@
 exec drop_object_if_exists('line_number_sq','sequence');
 create sequence line_number_sq;
 
-exec drop_object_if_exists('day??_example','table');
-create table day??_example (lineno number, linevalue varchar2(4000));
+exec drop_object_if_exists('day03_example','table');
+create table day03_example (lineno number, linevalue varchar2(4000));
 
-create or replace synonym input_data for day??_example;
+create or replace synonym input_data for day03_example;
 
 --insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'
 --');
 
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'467..114..');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'...*......');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'..35..633.');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'......#...');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'617*......');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'.....+.58.');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'..592.....');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'......755.');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'...$.*....');
+insert into input_data (lineno, linevalue) values (line_number_sq.nextval,'.664.598..');
+
 commit;
+
--- Day_03/Oracle/example_workbook.sql
+++ Day_03/Oracle/example_workbook.sql
@@ -1,6 +1,71 @@
 -- scratchpad for creating the solution
-create or replace synonym input_data for day??_example;
+create or replace synonym input_data for day03_example;
 
 select * from input_data;
 
--- Sample
+/*
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+
+
+General idea is check a box around the part numbers to see if a symble is in it
+find symbol positions
+find part number positions
+symbol x,y in -1,-1 begining to +1,+1 end
+
+sum the part numbers that have a symbol close by
+*/
+-- find engine_schematic size
+create or replace view schematic_size as (
+  select count(*) y, max(length(linevalue)) z
+  from input_data
+);
+select * from schematic_size; 
+--Y	Z
+--10	10
+
+-- not sure if needed, but go with it for now
+-- pad the engine schematic to get rid of constant bounds checking
+-- keep that in mind if we need to work in absolute values based on the original
+--   y isn't affected, but x is shifted by +1
+SELECT rpad('.',15,'.') "LPAD example" FROM DUAL;
+
+create or replace view engine_schematic as 
+select 0 lineno, rpad('.',x,'.') the_row from schematic_size
+union all select lineno, linevalue from input_data
+union all select y+1, rpad('.',x,'.') the_row from schematic_size
+/
+select * from engine_schematic;
+/*
+LINENO	THE_ROW
+0	..........
+1	467..114..
+2	...*......
+...
+*/
+
+-- maybe split lines into the non-'.' parts
+select 
+  *
+from engine_schematic e
+  ,lateral(select rownum id, column_value part, length(column_value) part_size  from table( string2rows(e.the_row,'.') )) n
+where n.part is not null
+/
+/*
+LINENO	THE_ROW	ID	PART	PART_SIZE
+4	......#...	7	#	1
+5	617*......	1	617*	4
+6	.....+.58.	6	+	1
+6	.....+.58.	7	58	2
+
+Almost right
+didn't think about symbols L,R of the part
+*/
