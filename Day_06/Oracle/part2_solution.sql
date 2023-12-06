e2a3d3cae3e0ba7d90ea064a9beb0b4307273bcc
Steven Patenaude <spatenaude@netspend.com>
2023-12-06 17:32:28 -0600

    solved quadratic equation which gives the solution

--- Day_06/Oracle/part2_solution.sql
+++ Day_06/Oracle/part2_solution.sql
@@ -1,4 +1,52 @@
--- Sample
---create or replace synonym input_data for day??_part2;
-create or replace synonym input_data for day??_part1;
+-- example solution worked as-is
+create or replace synonym input_data for day06_part1;
+
+-- need to remove the extra spaces to get the corrected input
+create or replace view input_data_no_spaces as select lineno, replace(linevalue,' ') linevalue from input_data;
+
+-- since spaces are gone, splitting on the ':'
+-- almost the same code as part1, results in one race
+-- lineno 1 has the time, lineno2 has the distance
+create or replace view parse1 as
+select i.lineno, i.linevalue, n.id, n.column_value
+from input_data_no_spaces i
+  ,lateral(select rownum id, column_value from table( string2rows(i.linevalue,':')) where column_value is not null) n
+;
+
+-- same code as part1
+create or replace view races as
+select id race_id, time_cv time, distance_cv as distance
+from (select lineno, id, column_value from parse1)
+pivot ( max(column_value) as cv for lineno in (1 as Time,2 as distance))
+where id != 1;
+/*
+RACE_ID	TIME	DISTANCE
+2	71530	940200
+*/
+
+/*
+generating all the moves is far too expensive, gotta be smarter
+remember observation from example workbench?
+  -- so distance is hold_seconds * (time-hold_seconds)
+So our holding beats the record distance when:
+  hold_seconds * (time-hold_seconds) - record_distance > 0
+  or: hold_seconds*time - (hold_seconds*hold_seconds) - record_distance > 0
+  or: hold_seconds*hold_seconds - hold_seconds*time + record_distance > 0
+That's a quadratic equation. The zeros are
+  hold_seconds = (time +- sqrt(time*time-4*distance))/2
+We need the number of seconds between those zeros.
+  Keep in mind we're only looking at whole numbers (seconds).
+  So round up from the first zero, round down from the last zero
+Moving things around to accomodate sql notation gives us this!
+*/
+select
+   round((time - sqrt(time*time-4*distance))/2+0.5) first_winning_race
+  ,trunc((time + sqrt(time*time-4*distance))/2)   last_winning_race
+  ,  trunc((time + sqrt(time*time-4*distance))/2)
+   - round((time - sqrt(time*time-4*distance))/2+0.5) diff
+  ,  trunc((time + sqrt(time*time-4*distance))/2)
+   - round((time - sqrt(time*time-4*distance))/2+0.5)
+   +1  num_in_interval
+from races;
+-- okay, num_in_interal is the answer!
 
--- Day_06/Oracle/part2_workbook.sql
+++ Day_06/Oracle/part2_workbook.sql
@@ -21,6 +21,64 @@ select * from races;
 -- going to be a lot, so just counting...
 select count(*) from races_by_hold_time;
 
--- oh, no multiplying, so that's the answer
-select count(*) answer from races_by_hold_time;
+-- shocking ran out of memory
+select * from races;
+/*
+RACE_ID	TIME	DISTANCE
+2	59688274	543102016641022
+*/
+-- remember distance is hold_seconds * (time-hold_seconds)
+-- s*t-s*s
+select LOG(2,543102016641022) from dual;
+select time, distance, (time/2)*(time-time/2) from races;
+-- thats a win, maybe binary search from there both up and down
 
+-- hmm, d=h*t-h*h; h*t=d+h*h; t=(d+h*h)/h; t=d/h+h
+-- d=h*t-h*h; h^2-th-d=0; a=1, b=-t, c=-d; h=(-t +- sqrt(-t*-t-4*-d))/2
+
+-- less than this, hold gives a loss
+select (time + sqrt(time*time-4*(distance))  )/2 from races;
+-- greater than this, hold gives a loss
+select (time - sqrt(time*time-4*(distance))  )/2 from races;
+
+select trunc((time + sqrt(time*time-4*(distance)))/2+0.5) from races;
+select trunc((time - sqrt(time*time-4*(distance)))/2) from races;
+
+select 
+  ,trunc((time + sqrt(time*time-4*(distance)))/2+0.5) first_winning_race
+  ,trunc((time - sqrt(time*time-4*(distance)))/2) last_winning_race
+  ,trunc((time - sqrt(time*time-4*(distance)))/2) - trunc((time + sqrt(time*time-4*(distance)))/2+0.5) diff
+  ,trunc((time - sqrt(time*time-4*(distance)))/2) - trunc((time + sqrt(time*time-4*(distance)))/2+0.5) + 1 num_in_interval
+from races;
+
+select
+  trunc((time - sqrt(time*time-4*(distance)))/2) first_winning_race
+  ,trunc((time + sqrt(time*time-4*(distance)))/2+0.5) last_winning_race
+  ,trunc((time + sqrt(time*time-4*(distance)))/2+0.5) - trunc((time - sqrt(time*time-4*(distance)))/2) diff
+  ,trunc((time + sqrt(time*time-4*(distance)))/2+0.5) - trunc((time - sqrt(time*time-4*(distance)))/2)+1 num_in_interval
+from races;
+-- interval = answer?
+--your answer is too high
+
+-- d=h*t-h*h
+-- h^2+d=h*t
+-- h^2-t*h+d=0; a=1, b=-t, c=d
+-- h=(-b +- sqrt(b*b-4*a*c))/2a
+-- h=(-(-t) +- sqrt((-t)*(-t)-4*1*d))/2*1
+-- (-(-time) +- sqrt((-time)*(-time)-4*1*distance))/2*1
+-- (time +- sqrt(time*time-4*distance))/2
+select
+(time - sqrt(time*time-4*distance))/2
+,(time + sqrt(time*time-4*distance))/2
+from races;
+
+select
+   round((time - sqrt(time*time-4*distance))/2+0.5) first_winning_race
+  ,trunc((time + sqrt(time*time-4*distance))/2)   last_winning_race
+  ,  trunc((time + sqrt(time*time-4*distance))/2)
+   - round((time - sqrt(time*time-4*distance))/2+0.5) diff
+  ,  trunc((time + sqrt(time*time-4*distance))/2)
+   - round((time - sqrt(time*time-4*distance))/2+0.5)
+   +1  num_in_interval
+from races;
+-- okay, num_in_interal is right
