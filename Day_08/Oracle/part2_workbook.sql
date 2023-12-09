0d9aecb027f3876cbbeb7a87e7cccd428a93e9c0
jungwolf <jungwolf@gmail.com>
2023-12-09 04:36:38 -0500

    part2

--- Day_08/Oracle/part2_workbook.sql
+++ Day_08/Oracle/part2_workbook.sql
@@ -2,7 +2,7 @@
 --create or replace synonym input_data for day??_part1;
 --create or replace synonym input_data for day??_part2;
 
-create or replace synonym input_data for day??_example;
-
+create or replace synonym input_data for day08_part2_example1;
+select * from input_data;
 
 
