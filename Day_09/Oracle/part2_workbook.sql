5f9597e3756a8e1c61426970f94c6e2aafd186c9
Steven Patenaude <spatenaude@netspend.com>
2023-12-15 15:52:40 -0600

    realized the trick, reverse the sequence order

--- Day_09/Oracle/part2_workbook.sql
+++ Day_09/Oracle/part2_workbook.sql
@@ -213,3 +213,46 @@ let's get more examples
 ?? value - lag(value,1) over (partition by history_id order by diff_level desc) diff_val_lag
 
 */
+
+
+create or replace synonym input_data for day09_part1;
+-- ug. I had the answer going forward, just needed to reverse the inputs
+-- row_number() gives me the current row number ordered in whatever way, not just as they are returned
+with sequences as (
+  select /*+ materialize */
+    to_number(i.lineno) history_id
+--    , n.id seq_num
+    , row_number() over (partition by to_number(i.lineno) order by id desc) seq_num
+    , to_number(n.column_value) value
+  from input_data i
+    ,lateral(select rownum id, column_value from table(string2rows(i.linevalue,' '))) n
+)
+, pyramid (history_id, seq_num, value, diff_level) as (
+  select history_id, seq_num, value, 0
+  from sequences
+
+  union all
+
+  select history_id, seq_num
+    , value - lag(value,1) over (partition by history_id order by seq_num)
+    , diff_level +1
+  from pyramid
+  where diff_level < 10000
+    and value is not null
+)
+/*select
+  rpad(' ',9*diff_level)
+    || listagg(to_char(value,'9999999'),' ') within group (order by seq_num)
+  , diff_level
+from pyramid
+group by history_id,diff_level
+*/
+--select sum(value) the_sum
+select sum(value) the_sum
+from (
+  select seq_num, value, diff_level
+    , max(seq_num) over (partition by diff_level) end_diff
+  from pyramid
+)
+where seq_num = end_diff
+/
