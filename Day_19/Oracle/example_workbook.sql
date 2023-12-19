eba05cf00de85e09e761cde844b3c06f7d4adf94
Steven Patenaude <spatenaude@netspend.com>
2023-12-19 17:32:31 -0600

    finished example

--- Day_19/Oracle/example_workbook.sql
+++ Day_19/Oracle/example_workbook.sql
@@ -301,3 +301,111 @@ with a (part_id, wf_name, wfr_order, r_attr, r_compare, r_value, value, wf_d, ex
 )
 select * from a;
 
+
+
+-- looking very good
+with a (part_id, wf_name, wfr_order, r_attr, r_compare, r_value, value, wf_d, expr, lvl) as (
+  select p.part_id, wf.wf_name, wf.wfr_order, wf.r_attr, wf.r_compare, wf.r_value, p.value, wf.wf_destination
+  , case wf.r_compare
+      when '<' then nvl((select wf.wf_destination from dual where p.value < wf.r_value),wf.wf_name)
+      when '=' then nvl((select wf.wf_destination from dual where p.value = wf.r_value),wf.wf_name)
+      when '>' then nvl((select wf.wf_destination from dual where p.value > wf.r_value),wf.wf_name)
+      when 'D' then wf.wf_destination
+    end expr
+  , 0
+  from parts_attrs p, workflow_steps wf
+  where p.part_id in (14,13)
+    and wf.wf_name = 'in'
+    and wf.wfr_order = 1
+    and wf.r_attr = p.attr
+
+  union all
+
+  select a.part_id, wf.wf_name, wf.wfr_order, wf.r_attr, wf.r_compare, wf.r_value, a.value, wf.wf_destination
+  , case wf.r_compare
+      when '<' then nvl((select wf.wf_destination from dual where p.value < wf.r_value),wf.wf_name)
+      when '=' then nvl((select wf.wf_destination from dual where p.value = wf.r_value),wf.wf_name)
+      when '>' then nvl((select wf.wf_destination from dual where p.value > wf.r_value),wf.wf_name)
+      when 'D' then wf.wf_destination
+    end expr
+    , lvl+1
+  from parts_attrs p, workflow_steps wf, a
+  where a.part_id = p.part_id (+)
+    and wf.wf_name = a.expr
+-- if on a new destination, restart order
+    and wf.wfr_order = decode(a.expr,a.wf_name,a.wfr_order+1,1)
+    and wf.r_attr = p. attr (+)
+    and a.lvl < 10
+)
+--select * from a;
+select * from a
+where wfr_order = 1
+order by part_id, lvl, wfr_order;
+
+
+-- would be nice if A and R were terminal workflows, instead of just null rows
+create or replace view workflow_rules as
+select lineno wf_id
+--  , instr(linevalue,'{')
+  , substr(linevalue,1,instr(linevalue,'{')-1) wf_name
+  , replace(substr(linevalue,instr(linevalue,'{')+1),'}') wf_rule
+from workflows_and_parts
+where linevalue is not null
+  and group_id = 0
+union all select -1,'A',null from dual
+union all select -2,'R',null from dual
+;
+select * from workflow_rules;
+--WF_ID	WF_NAME	WF_RULE
+--1	px	a<2006:qkq,m>2090:A,rfg
+--2	pv	a>1716:R,A
+
+
+
+with a (part_id, wf_name, wfr_order, r_attr, r_compare, r_value, value, wf_d, expr, lvl) as (
+  select p.part_id, wf.wf_name, wf.wfr_order, wf.r_attr, wf.r_compare, wf.r_value, p.value, wf.wf_destination
+  , case wf.r_compare
+      when '<' then nvl((select wf.wf_destination from dual where p.value < wf.r_value),wf.wf_name)
+      when '=' then nvl((select wf.wf_destination from dual where p.value = wf.r_value),wf.wf_name)
+      when '>' then nvl((select wf.wf_destination from dual where p.value > wf.r_value),wf.wf_name)
+      when 'D' then wf.wf_destination
+    end expr
+  , 0
+  from parts_attrs p, workflow_steps wf
+  where 1=1
+--    and p.part_id in (14,13)
+    and wf.wf_name = 'in'
+    and wf.wfr_order = 1
+    and wf.r_attr = p.attr
+
+  union all
+
+  select a.part_id, wf.wf_name, wf.wfr_order, wf.r_attr, wf.r_compare, wf.r_value, a.value, wf.wf_destination
+  , case wf.r_compare
+      when '<' then nvl((select wf.wf_destination from dual where p.value < wf.r_value),wf.wf_name)
+      when '=' then nvl((select wf.wf_destination from dual where p.value = wf.r_value),wf.wf_name)
+      when '>' then nvl((select wf.wf_destination from dual where p.value > wf.r_value),wf.wf_name)
+      when 'D' then wf.wf_destination
+    end expr
+    , lvl+1
+  from parts_attrs p, workflow_steps wf, a
+  where a.part_id = p.part_id (+)
+    and wf.wf_name = a.expr
+-- if on a new destination, restart order
+    and wf.wfr_order = decode(a.expr,a.wf_name,a.wfr_order+1,1)
+    and wf.r_attr = p. attr (+)
+    and a.lvl < 10
+)
+/*select * from a
+where wfr_order = 1
+order by part_id, lvl, wfr_order;
+*/
+/*select part_id, listagg(wf_name,' -> ') within group (order by lvl)
+from a
+group by part_id;*/
+/*select part_id, wf_name
+from a
+where wf_name in ('A','R');*/
+--select a.part_id, p.attr, p.value from a,parts_attrs p where wf_name in ('A','R') and a.part_id = p.part_id;
+select sum(p.value) from a,parts_attrs p where wf_name in ('A') and a.part_id = p.part_id;
+--19114
