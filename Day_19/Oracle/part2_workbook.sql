e1a3609e79565ed3cfe69ac8f30c7e57ba3d423d
jungwolf <jungwolf@gmail.com>
2023-12-31 19:30:31 -0500

    maybe check critical xmas values to test A or R

--- Day_19/Oracle/part2_workbook.sql
+++ Day_19/Oracle/part2_workbook.sql
@@ -116,3 +116,198 @@ with a (wf_name, wfr_order, r_attr, r_compare, r_value, wf_destination, lvl) as 
 
 )
 select * from a;
+
+
+with a (wf_name, wfr_order, r_attr, r_compare, r_value, wf_destination, lvl, the_path) as (
+  select wf_name, wfr_order, r_attr, r_compare, r_value, wf_destination, 1, wf_name||'.'||wfr_order||'\'
+  from workflow_steps
+  where wf_name = 'in'
+
+  union all
+
+  select w.wf_name, w.wfr_order, w.r_attr, w.r_compare, w.r_value, w.wf_destination, a.lvl+1, a.the_path||w.wf_name||'.'||w.wfr_order||'\'
+  from a, workflow_steps w
+  where a.wf_destination = w.wf_name
+    and w.wf_name not in ('A','R')
+
+)
+select * 
+from a
+where wf_destination = 'A'
+order by the_path;
+-- '
+-- something odd with notepad++ quoting so added a single quote
+
+-- let's assume we don't revisit a node
+-- hmm, how about listing all the boundary values?
+select * from workflows_and_parts;
+select * from workflow_sub1;
+select * from workflow_steps;
+
+create or replace view critical_points as
+with spread as (select -1 offset from dual union all select 1 from dual union all select 0 from dual)
+select r_attr, r_value+offset from workflow_steps, spread
+where r_compare not in ('D')
+order by 1, 2;
+select * from critical_points;
+
+create or replace view critical_points as
+with spread as (select -1 offset from dual union all select 1 from dual union all select 0 from dual)
+select r_attr attr, r_value+offset value from workflow_steps, spread
+where r_compare not in ('D')
+order by 1, 2;
+select * from critical_points;
+
+with spread as (select -1 offset from dual union all select 1 from dual union all select 0 from dual)
+, critical_points as (
+  select /*+ materialize */ r_attr attr, r_value+offset value from workflow_steps, spread
+  where r_compare not in ('D')
+)
+select * 
+from critical_points x
+  , critical_points m
+  , critical_points a
+  , critical_points s
+where x.attr='x'
+  and m.attr='m'
+  and a.attr='a'
+  and s.attr='s'
+;
+
+-- too many
+with spread as (select -1 offset from dual union all select 1 from dual union all select 0 from dual)
+, a (wf_name, wfr_order, r_attr, r_compare, r_value, wf_destination, lvl, the_path) as (
+  select wf_name, wfr_order, r_attr, r_compare, r_value, wf_destination, 1, wf_name||'.'||wfr_order||'/'
+  from workflow_steps
+  where wf_name = 'in'
+
+  union all
+
+  select w.wf_name, w.wfr_order, w.r_attr, w.r_compare, w.r_value, w.wf_destination, a.lvl+1, a.the_path||w.wf_name||'.'||w.wfr_order||'/'
+  from a, workflow_steps w
+  where a.wf_destination = w.wf_name
+    and w.wf_name not in ('A','R')
+
+)
+select * 
+from a
+--where wf_destination = 'A'
+order by the_path;
+
+with spread as (select -1 offset from dual union all select 1 from dual union all select 0 from dual)
+, a (wf_name, wfr_order, r_attr, r_compare, r_value, wf_destination, lvl, the_path) as (
+  select wf_name, wfr_order, r_attr, r_compare, r_value, wf_destination, 1, wf_name||'.'||wfr_order||'/'
+  from workflow_steps
+  where wf_name = 'in'
+
+  union all
+
+  select w.wf_name, w.wfr_order, w.r_attr, w.r_compare, w.r_value, w.wf_destination, a.lvl+1, a.the_path||w.wf_name||'.'||w.wfr_order||'/'
+  from a, workflow_steps w
+  where a.wf_destination = w.wf_name
+    and w.wf_name not in ('A','R')
+
+)
+select 
+--  *
+--  r_attr, r_value+offset 
+--  unique r_attr, r_value+offset
+  r_attr, count(*)
+from a, spread
+--where wf_destination = 'A'
+where r_attr is not null
+group by r_attr
+--order by 1,2
+/
+/* count(*) 3456
+ unique 3020
+s	858
+a	963
+x	822
+m	813
+*/
+
+
+
+
+
+
+with spread as (select -1 offset from dual union all select 1 from dual union all select 0 from dual)
+, a (wf_name, wfr_order, r_attr, r_compare, r_value, wf_destination, lvl, the_path) as (
+  select wf_name, wfr_order, r_attr, r_compare, r_value, wf_destination, 1, wf_name||'.'||wfr_order||'/'
+  from workflow_steps
+  where wf_name = 'in'
+
+  union all
+
+  select w.wf_name, w.wfr_order, w.r_attr, w.r_compare, w.r_value, w.wf_destination, a.lvl+1, a.the_path||w.wf_name||'.'||w.wfr_order||'/'
+  from a, workflow_steps w
+  where a.wf_destination = w.wf_name
+    and w.wf_name not in ('A','R')
+    and w.r_attr is not null
+)
+, critical_values as (
+  select 
+    r_attr attr, r_value+offset value
+  from a, spread
+  where r_attr is not null
+)
+select count(*) 
+from critical_values x
+  , critical_values m
+  , critical_values a
+  , critical_values s
+where x.attr='x'
+  and m.attr='m'
+  and a.attr='a'
+  and s.attr='s'
+/
+
+
+
+
+with spread as (select -1 offset from dual union all select 1 from dual union all select 0 from dual)
+, critical_values_x as (
+  select /*+ materialize */
+    unique r_attr attr, r_value+offset value
+  from workflow_steps, spread
+  where r_attr = 'x'
+)
+, critical_values_m as (
+  select /*+ materialize */
+    unique r_attr attr, r_value+offset value
+  from workflow_steps, spread
+  where r_attr = 'm'
+)
+, critical_values_a as (
+  select /*+ materialize */
+    unique r_attr attr, r_value+offset value
+  from workflow_steps, spread
+  where r_attr = 'a'
+)
+, critical_values_s as (
+  select /*+ materialize */
+    unique r_attr attr, r_value+offset value
+  from workflow_steps, spread
+  where r_attr = 's'
+)
+select count(*) from (
+select unique *
+from
+   critical_values_x -- 739
+,   critical_values_m -- 728
+,   critical_values_a -- 806
+--  critical_values_s -- 747
+where 1=1
+)
+--order by 1,2
+/
+
+select * from workflows_and_parts;
+select * from workflow_sub1;
+select * from workflow_steps;
+select * from workflow_steps where r_attr is not null;
+select r_attr, count(*) from workflow_steps where r_attr is not null group by r_attr;
+
+-- well, still looking at about a trillion values...
+
